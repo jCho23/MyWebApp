71343601767ac406dd61f3825b0d57182462917e
June Cho <junecho.ucb@gmail.com>
2018-03-03 18:56:07 -0800

    added NEw Controller

--- MyWebApp/Controllers/ProductsController.cs
+++ MyWebApp/Controllers/ProductsController.cs
@@ -1,37 +1,119 @@
-﻿using MyWebApp.Models;
-using System;
+﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web.Http;
+using System.Web.Http.Description;
+using MyWebApp.Models;
 
 namespace MyWebApp.Controllers
 {
     public class ProductsController : ApiController
     {
-        Product[] products = new Product[]
-        {
-            new Product { Id= 1, ProductName= "Jordans", Price=1},
-            new Product { Id= 2, ProductName= "Boost", Price= 3.75M},
-            new Product { Id= 3, ProductName= "Nike", Price=16.99M}
-        };
+        private MyWebAppContext db = new MyWebAppContext();
 
-        public IEnumerable<Product> GetAllProducts()
+        // GET: api/Products
+        public IQueryable<Product> GetProducts()
         {
-            return products;
+            return db.Products;
         }
 
-        public IHttpActionResult GetProduct(int id)
+        // GET: api/Products/5
+        [ResponseType(typeof(Product))]
+        public async Task<IHttpActionResult> GetProduct(int id)
         {
-            var product = products.FirstOrDefault((x) => x.Id == id);
+            Product product = await db.Products.FindAsync(id);
             if (product == null)
             {
                 return NotFound();
             }
+
             return Ok(product);
         }
 
+        // PUT: api/Products/5
+        [ResponseType(typeof(void))]
+        public async Task<IHttpActionResult> PutProduct(int id, Product product)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
 
+            if (id != product.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(product).State = EntityState.Modified;
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProductExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Products
+        [ResponseType(typeof(Product))]
+        public async Task<IHttpActionResult> PostProduct(Product product)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Products.Add(product);
+            await db.SaveChangesAsync();
+
+            return CreatedAtRoute("DefaultApi", new { id = product.Id }, product);
+        }
+
+        // DELETE: api/Products/5
+        [ResponseType(typeof(Product))]
+        public async Task<IHttpActionResult> DeleteProduct(int id)
+        {
+            Product product = await db.Products.FindAsync(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            db.Products.Remove(product);
+            await db.SaveChangesAsync();
+
+            return Ok(product);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool ProductExists(int id)
+        {
+            return db.Products.Count(e => e.Id == id) > 0;
+        }
     }
 }
